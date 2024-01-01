db04ed436e10a5ef06f3710cd78a2c5fbbd9b87f
muhammed suhail <wwwsuhail883@gmail.com>
2024-01-01 22:24:13 +0530

    some updates

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000, function () {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.listen(3002, function () {
+  console.log(`Server is running on http://localhost:${3002}`);
 });
+//hai
